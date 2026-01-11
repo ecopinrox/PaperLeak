f26ce815483122c44980efa1a20f50eefa2ef493
Akhil Appayya <ecopin12345@gmail.com>
2026-01-11 18:31:32 +0530

    added artifact (working name)

--- Paper Leak/Assets/Scripts/Items/ItemTypes/Artifact.cs
+++ Paper Leak/Assets/Scripts/Items/ItemTypes/Artifact.cs
@@ -2,10 +2,30 @@ using UnityEngine;
 
 public class Artifact : Item
 {
+    [Header("Aiming")]
+    [SerializeField] float radius = 10f;
+    [SerializeField] LayerMask targetBlockingMask;
+    [SerializeField] LayerMask rayBlockingMask;
+
+    [Header("Teleport Sound")]
+    [SerializeField] GameObject soundDistractionPrefab;
+    [SerializeField] SoundData teleportSFX;
+
     public async override Awaitable<bool> Use()
     {
         Transform playerTransform = PlayerController.Instance.transform;
-        Awaitable s;
+        PlayerMovement playerMovement = PlayerController.Instance.GetComponent<PlayerMovement>();
+
+        Vector2Int? target = await AimingController.Instance.Aim(radius, targetBlockingMask, rayBlockingMask);
+        if (target == null)
+        {
+            return false;
+        }
+
+        playerMovement.SnapToPosition((Vector2Int)target);
+
+        SoundDistraction soundDistractionInstance = Instantiate(soundDistractionPrefab, playerTransform.position, Quaternion.identity).GetComponent<SoundDistraction>();
+        soundDistractionInstance.PlaySound(teleportSFX);
 
         return true;
     }
