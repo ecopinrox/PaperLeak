31dd9225c20ea94c1ab19b6b72211eae9476f322
Akhil Appayya <ecopin12345@gmail.com>
2026-07-10 12:15:20 +0530

    Color changes on interaction

--- Paper Leak/Assets/Scripts/Interactible/ConversationPoint.cs
+++ Paper Leak/Assets/Scripts/Interactible/ConversationPoint.cs
@@ -4,16 +4,31 @@ public class ConversationPoint : Interactible
 {
     bool interacted = false;
 
+    [SerializeField] Color interactedColor;
+
+    SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
     public override void Interact()
     {
         if (!interacted)
         {
             Debug.Log("conversation start");
             interacted = true;
+            SwitchColor();
         }
         else
         {
             Debug.Log("conversation start again");
         }
+    }
+
+    void SwitchColor()
+    {
+        spriteRenderer.color = interactedColor;
     }
 }
