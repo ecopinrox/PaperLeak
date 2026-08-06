80bc25e97d0eff3bab824c5716e62e19ecf235ba
Akhil Appayya <ecopin12345@gmail.com>
2026-08-06 15:37:14 +0530

    Fixed sprite sorting issues with checkpoint and door

--- Paper Leak/Assets/Scripts/Interactible/Checkpoint.cs
+++ Paper Leak/Assets/Scripts/Interactible/Checkpoint.cs
@@ -1,10 +1,19 @@
+using UnityEngine;
+
 public class Checkpoint : Interactible
 {
     UIManager uiManager;
+    SpriteRenderer spriteRenderer;
 
     private void Awake()
     {
         uiManager = FindFirstObjectByType<UIManager>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        spriteRenderer.sortingOrder = -Mathf.RoundToInt(transform.position.y);
     }
 
     public override void Interact()
