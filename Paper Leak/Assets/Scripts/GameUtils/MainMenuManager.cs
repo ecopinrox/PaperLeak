ca43bb796862a7f16e25f90fc591b7de42293e4a
Akhil Appayya <ecopin12345@gmail.com>
2026-08-06 17:26:31 +0530

    Added a button to clear saves from the main menu

--- Paper Leak/Assets/Scripts/GameUtils/MainMenuManager.cs
+++ Paper Leak/Assets/Scripts/GameUtils/MainMenuManager.cs
@@ -3,4 +3,6 @@ using UnityEngine;
 public class MainMenuManager : MonoBehaviour
 {
     public void StartGame() => LevelManager.Instance.StartGame();
+
+    public void ClearSave() => LevelManager.Instance.DeleteSave();
 }
