214294eb8e3412b623b42907a4e6875ef2e759b2
Akhil Appayya <ecopin12345@gmail.com>
2026-08-07 15:43:03 +0530

    Added cartoon effect for intro

--- Paper Leak/Assets/Scripts/UI/CartoonEffectManager.cs
+++ Paper Leak/Assets/Scripts/UI/CartoonEffectManager.cs
@@ -1,11 +1,44 @@
 using UnityEngine;
+using System.Threading.Tasks;
 
 public class CartoonEffectManager : MonoBehaviour
 {
+    [SerializeField] GameObject mask;
     [SerializeField] SpriteMask hole;
+    [SerializeField] float outerPhaseScale = 70f;
+    [SerializeField] float innerPhaseScale = 3f;
+    [SerializeField] float outerPhaseTime = 3f;
+    [SerializeField] float outerHoldTime = 3f;
+    [SerializeField] float innerPhaseTime = 1f;
+    [SerializeField] float innerHoldTime = 0.5f;
+
+    private void Start()
+    {
+        mask.SetActive(true);
+        _ = ExpandHole();
+    }
 
     private void FixedUpdate()
     {
         hole.transform.position = PlayerController.Instance.transform.position;
+    }
+
+    async Awaitable ExpandHole()
+    {
+        await Task.Delay((int)(1000 * innerHoldTime));
+        await LerpScale(innerPhaseScale, innerPhaseTime);
+        await Task.Delay((int)(1000 * outerHoldTime));
+        await LerpScale(outerPhaseScale, outerPhaseTime);
+    }
+
+    async Awaitable LerpScale(float end, float duration)
+    {
+        float start = hole.transform.localScale.x;
+
+        for (float time = 0; time < duration; await Awaitable.WaitForSecondsAsync(Time.unscaledDeltaTime), time += Time.unscaledDeltaTime)
+        {
+            float value = time / duration;
+            hole.transform.localScale = Vector3.one * Mathf.Lerp(start, end, value); 
+        }
     }
 }
