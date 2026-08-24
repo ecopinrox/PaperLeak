4de71e0ff4f14d268d8cba049c413bb4fe138596
Akhil Appayya <ecopin12345@gmail.com>
2026-08-24 16:09:33 +0530

    Added challenges button to main menu

--- Paper Leak/Assets/Scripts/GameUtils/MainMenuManager.cs
+++ Paper Leak/Assets/Scripts/GameUtils/MainMenuManager.cs
@@ -2,7 +2,22 @@ using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [SerializeField] GameObject challengesButton;
+
+    private void Start()
+    {
+        if(!LevelManager.Instance.CheckForOverallStats())
+        {
+            challengesButton.SetActive(false);
+        }
+    }
+
     public void StartGame() => LevelManager.Instance.StartGame();
 
     public void ClearSave() => LevelManager.Instance.DeleteSave();
+
+    public void ShowChallenges()
+    {
+        Debug.Log("Show challenges");
+    }
 }
